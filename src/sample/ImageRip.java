7dd07d731b55bd2a22936c6ab12a12b59ec4c701
robhewison <robhewison1@gmail.com>
2020-02-18 05:09:54 -0500

    Added basic download algorithm (doesn't work yet)

--- src/sample/ImageRip.java
+++ src/sample/ImageRip.java
@@ -1,14 +1,21 @@
 package sample;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class ImageRip {
 
+    private static final String IMAGE_HOME = "/Users/robhewison";
     public String website;
 
     public ImageRip(String website) {
@@ -71,11 +78,43 @@ public class ImageRip {
         }
     }
 
-    public void downloadImages(ArrayList<String> toDownload) {
-
+    public String downloadImages(String imageUrl, String fileName, String relativePath) {
+        String imagePath = null;
+        try {
+            byte[] bytes = Jsoup.connect(imageUrl).ignoreContentType(true).execute().bodyAsBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(bytes);
+            String rootTargetDirectory = IMAGE_HOME + "/"+relativePath;
+            imagePath = rootTargetDirectory + "/"+fileName;
+            saveByteBufferImage(buffer, rootTargetDirectory, fileName);
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return imagePath;
     }
 
+    public static void saveByteBufferImage(ByteBuffer imageDataBytes, String rootTargetDirectory, String savedFileName) {
+        String uploadInputFile = rootTargetDirectory + "/"+savedFileName;
 
+        File rootTargetDir = new File(rootTargetDirectory);
+        if (!rootTargetDir.exists()) {
+            boolean created = rootTargetDir.mkdirs();
+            if (!created) {
+                System.out.println("Error while creating directory for location- "+rootTargetDirectory);
+            }
+        }
+        String[] fileNameParts = savedFileName.split("\\.");
+        String format = fileNameParts[fileNameParts.length-1];
 
+        File file = new File(uploadInputFile);
+        BufferedImage bufferedImage;
 
+        InputStream in = new ByteArrayInputStream(imageDataBytes.array());
+        try {
+            bufferedImage = ImageIO.read(in);
+            ImageIO.write(bufferedImage, format, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
